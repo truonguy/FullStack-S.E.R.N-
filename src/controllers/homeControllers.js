4513dffd5c1eb0bf16db8e1c7fe6cc6ac5471e8f
truonguy <truonguy123tx@gmail.com>
2023-12-06 11:12:41 +0700

    FullStack SERN

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -23,8 +23,43 @@ let postCRUD = async(req, res) => {
     return res.send('postCRUD form server'); 
 }
 
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log('--------------------------------');
+    console.log(data);
+    console.log('--------------------------------');
+    
+    return res.render('display-crud.ejs', {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if(userId){
+        let userData = await CRUDService.getUserInfoById(userId);
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+
+    }else{
+        return res.send('users not found');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;//lay tat ca input editCRUD.ejs
+    let allUsers = await CRUDService.updateUserData(data); //lay lai tat ca danh sach moi
+    return res.render('display-crud.ejs', {
+        dataTable: allUsers
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
